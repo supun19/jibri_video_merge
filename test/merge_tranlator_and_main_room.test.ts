f7761a7302e8bd932094053817193b66b8320b57
Supun Madushanka <supun.madushanka@aircanada.ca>
2025-08-09 23:01:14 +0530

    Initial commit

---
+++ test/merge_tranlator_and_main_room.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MergeTranlatorAndMainRoom from '../lib/merge_tranlator_and_main_room-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/merge_tranlator_and_main_room-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MergeTranlatorAndMainRoom.MergeTranlatorAndMainRoomStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
